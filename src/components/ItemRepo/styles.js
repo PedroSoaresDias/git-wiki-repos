2bd0834560b21e10a28f7f685a00bfd564460c8f
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-08-09 19:38:15 -0300

    First commit

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+
+    button.remover {
+        color: #FF0000;
+        margin-top: 20px;
+        border: none;
+        background-color: transparent;
+        font-size: 16px;
+        text-decoration: underline;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
